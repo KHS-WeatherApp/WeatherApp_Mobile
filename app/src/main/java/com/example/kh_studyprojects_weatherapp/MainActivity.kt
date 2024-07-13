145ffb39aa4b9d603f2314436717a9913b70c388
Hyodong Kim <gyehd1230@naver.com>
2024-07-13 15:40:54 +0900

    test

---
+++ app/src/main/java/com/example/kh_studyprojects_weatherapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kh_studyprojects_weatherapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kh_studyprojects_weatherapp.databinding.ActivityMainBinding
+
+// 사용하지 않는 변수, 변경된 파라미터 이름 등에 대한 컴파일러 경고를 억제합니다.
+class MainActivity : AppCompatActivity() {
+
+    // 뷰 바인딩을 위한 널 가능한(private) 변수를 선언합니다.
+    private var _binding: ActivityMainBinding? = null
+    // _binding 변수에 대한 널이 아닌 접근자를 제공합니다.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
