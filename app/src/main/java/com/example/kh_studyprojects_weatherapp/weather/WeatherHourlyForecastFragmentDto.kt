0e6db523718b9e88b24b18d5f000658574201cde
Myodong <gyehd1230@naver.com>
2024-08-16 00:44:28 +0900

    임시커밋 할일 - RecyclerView 어댑터 핸들러 수정 - item 가로세로 xml  하나씩 만들기 - Switch 버튼 기능 구현

--- app/src/main/java/com/example/kh_studyprojects_weatherapp/weather/WeatherHourlyForecastFragmentDto.kt
+++ app/src/main/java/com/example/kh_studyprojects_weatherapp/weather/WeatherHourlyForecastFragmentDto.kt
@@ -2,4 +2,9 @@ package com.example.kh_studyprojects_weatherapp.weather
 
 class WeatherHourlyForecastFragmentDto (
 
+    val tvPmPa: String?,
+    val tvHour: String?,
+    val probability: String?,
+    val precipitation: String?,
+    val temperature: String?,
 )
