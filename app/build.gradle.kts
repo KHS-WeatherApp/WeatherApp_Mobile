fbfcdd148f6ff5551788911e70fcfed4c09ce432
wldbs <wldbs4166@naver.com>
2024-12-27 09:29:06 +0900

    지윤 1227일자 commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,51 +1,59 @@
 plugins {
-    alias(libs.plugins.android.application)         // 안드로이드 애플리케이션 플러그인을 사용합니다.
-    alias(libs.plugins.jetbrains.kotlin.android)    // 코틀린과 안드로이드를 함께 사용하기 위한 플러그인을 사용합니다.
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
-    namespace = "com.example.kh_studyprojects_weatherapp" // 앱의 유니크한 네임스페이스를 설정합니다.
-    compileSdk = 34 // 컴파일에 사용될 SDK 버전을 지정합니다.
+    namespace = "com.example.kh_studyprojects_weatherapp"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.kh_studyprojects_weatherapp" // 앱의 고유 ID를 설정합니다.
-        minSdk = 30             // 앱이 지원하는 최소 안드로이드 버전을 지정합니다.
-        targetSdk = 34          // 앱이 타겟으로 하는 안드로이드 SDK 버전을 지정합니다.
-        versionCode = 1         // 앱 버전 코드, 앱 업데이트 시 변경되어야 합니다.
-        versionName = "1.0.0"   // 앱 버전 이름, 사용자에게 보여지는 앱 버전입니다.
+        applicationId = "com.example.kh_studyprojects_weatherapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" // 테스트를 실행할 때 사용할 러너를 지정합니다.
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false // 코드 난독화 여부를 설정합니다. false는 난독화하지 않음을 의미합니다.
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
-            ) // Proguard를 사용하여 코드 보호 규칙 파일을 지정합니다.
+            )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8 // 소스 코드의 자바 호환성 버전을 지정합니다.
-        targetCompatibility = JavaVersion.VERSION_1_8 // 타겟 자바 호환성 버전을 지정합니다.
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8" // 코틀린 코드의 JVM 타겟 버전을 지정합니다.
+        jvmTarget = "1.8"
     }
     buildFeatures {
-        viewBinding = true // 뷰 바인딩을 활성화합니다. XML 레이아웃과 코드를 더 쉽게 연결할 수 있습니다.
-        dataBinding = true // 데이터 바인딩을 활성화하여 뷰와 모델 데이터 간의 연결을 도와줍니다.
+        buildConfig = true
     }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)                  // 안드로이드 KTX 코어 라이브러리를 포함합니다.
-    implementation(libs.androidx.appcompat)                 // 앱 호환성을 위한 AppCompat 라이브러리를 포함합니다.
-    implementation(libs.material)                           // 구글의 Material 디자인 컴포넌트를 포함합니다.
-    implementation(libs.androidx.activity)                  // 안드로이드 액티비티 관련 라이브러리를 포함합니다.
-    implementation(libs.androidx.constraintlayout)          // 레이아웃 제약 조건을 위한 라이브러리를 포함합니다.
-    testImplementation(libs.junit)                          // JUnit 테스트 프레임워크를 포함합니다.
-    androidTestImplementation(libs.androidx.junit)          // 안드로이드 JUnit 테스트를 위한 라이브러리를 포함합니다.
-    androidTestImplementation(libs.androidx.espresso.core)  // UI 테스트를 위한 Espresso 코어 라이브러리를 포함합니다.
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+    implementation(libs.retrofit.gson.converter)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    // OkHttp 및 Logging Interceptor 의존성
+    implementation(libs.okhttp)
+    implementation(libs.loggingInterceptor)
 }
--- app/src/main/java/com/example/kh_studyprojects_weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/kh_studyprojects_weatherapp/MainActivity.kt
@@ -1,29 +1,28 @@
 package com.example.kh_studyprojects_weatherapp
 
-import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.kh_studyprojects_weatherapp.databinding.ActivityMainBinding
 
-// 사용하지 않는 변수, 변경된 파라미터 이름 등에 대한 컴파일러 경고를 억제합니다.
+
 class MainActivity : AppCompatActivity() {
-
-    // 뷰 바인딩을 위한 널 가능한(private) 변수를 선언합니다.
-    private var _binding: ActivityMainBinding? = null
-    // _binding 변수에 대한 널이 아닌 접근자를 제공합니다.
-    private val binding get() = _binding!!
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.app_background_color)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, systemBarsInsets.top, 0, 0)
             insets
         }
+        //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        //window.statusBarColor = resources.getColor(R.color.black)
     }
 }
