fab0251737f5f1e25226bd779e7c4ff90e53c790
Hyodong Kim <gyehd1230@gmail.com>
2025-01-05 14:53:32 +0900

    미세먼지 프래그먼트, 세팅프래그먼트 임시커밋

---
+++ app/src/main/java/com/example/kh_studyprojects_weatherapp/presentation/finedust/FinedustFragment.kt
@@ -0,0 +1,54 @@
+package com.example.kh_studyprojects_weatherapp.particulatematter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
+import com.example.kh_studyprojects_weatherapp.R
+import com.example.kh_studyprojects_weatherapp.databinding.ParticulateMatterFragmentBinding
+
+class ParticulateMatterFragment : Fragment() {
+
+    private var _binding: ParticulateMatterFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ParticulateMatterFragmentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        //cl01 클릭 시 날씨 화면 전환
+        root.findViewById<ConstraintLayout>(R.id.clNav01).setOnClickListener{
+            it.findNavController().navigate(R.id.action_particulateMatterFragment_to_weatherFragment)
+            Toast.makeText(context, "날씨 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        //tv_setting 클릭 시 설정 화면 전환
+        root.findViewById<ConstraintLayout>(R.id.clNavOval).setOnClickListener{
+            it.findNavController().navigate(R.id.action_particulateMatterFragment_to_settingFragment)
+            Toast.makeText(context, "설정 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        //cl03 클릭 시 미세먼지 화면 전환
+        root.findViewById<ConstraintLayout>(R.id.clNav03).setOnClickListener{
+            it.findNavController().navigate(R.id.action_particulateMatterFragment_self2)
+            Toast.makeText(context, "미세먼지 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        // Inflate the layout for this fragment
+        return root
+    }
+
+}
---
+++ app/src/main/java/com/example/kh_studyprojects_weatherapp/presentation/setting/SettingFragment.kt
@@ -0,0 +1,49 @@
+
+package com.example.kh_studyprojects_weatherapp.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
+import com.example.kh_studyprojects_weatherapp.R
+
+class SettingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.setting_fragment, container, false)
+
+        //cl01 클릭 시 날씨 화면 전환
+        view.findViewById<ConstraintLayout>(R.id.clNav01).setOnClickListener{
+            it.findNavController().navigate(R.id.action_settingFragment_to_weatherFragment)
+            Toast.makeText(context, "날씨 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        //tv_setting 클릭 시 설정 화면 전환
+        view.findViewById<ConstraintLayout>(R.id.clNavOval).setOnClickListener{
+            it.findNavController().navigate(R.id.action_settingFragment_self2)
+            Toast.makeText(context, "설정 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        //cl03 클릭 시 미세먼지 화면 전환
+        view.findViewById<ConstraintLayout>(R.id.clNav03).setOnClickListener{
+            it.findNavController().navigate(R.id.action_settingFragment_to_particulateMatterFragment)
+            Toast.makeText(context, "미세먼지 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+}
