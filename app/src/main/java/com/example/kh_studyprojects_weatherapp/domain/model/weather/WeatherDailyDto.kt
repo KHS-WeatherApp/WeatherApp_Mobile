bf4d9a2de60a91ff1780a47976d795fd020eddfc
wldbs <wldbs4166@naver.com>
2025-03-23 16:47:28 +0900

    daily 화면 수정

--- app/src/main/java/com/example/kh_studyprojects_weatherapp/domain/model/weather/WeatherDailyDto.kt
+++ app/src/main/java/com/example/kh_studyprojects_weatherapp/domain/model/weather/WeatherDailyDto.kt
@@ -9,7 +9,10 @@ data class WeatherDailyDto(
     val minTemp: String,
     val maxTemp: String,
     val weatherCode: Int,
-    var isVisible: Boolean = true
+    var isVisible: Boolean = true,
+    // 기존 필드들...
+    val globalMinTemp: Double, // 전체 기간 최저 온도
+    val globalMaxTemp: Double  // 전체 기간 최고 온도
 ) {
     enum class Type {
         TODAY, YESTERDAY, OTHER
